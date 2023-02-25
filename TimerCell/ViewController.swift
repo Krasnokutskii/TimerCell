9bc7112bcf8c83161c30c56ca3fccc0281e4f100
Krasnokutskii Iaroslav <Yroslavkr1995@gmail.com>
2023-02-25 17:22:19 +0100

    Initial Commit

---
+++ TimerCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimerCell
+//
+//  Created by Yaroslav Krasnokutskiy on 25.2.23..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
